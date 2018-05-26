fd2ae9b8cf0f54a2366af147c62ea961a91aea38
abbas886 <abbas886@yahoo.com>
2018-05-26 10:29:11 +0530

    //it should return String , not void

--- src/com/cts/program/StringReverse.java
+++ src/com/cts/program/StringReverse.java
@@ -1,7 +1,11 @@
 package com.cts.program;
-
+//Class name should like MyString  OR String
 public class StringReverse {
+	//method name should like revers
+	//it should return String , not void
+	//variable name should not consist of digits
 public void string(String str1)
+	
 {
 	
 	char ch[]=str1.toCharArray();
