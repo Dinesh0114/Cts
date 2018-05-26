194e3637113c2ca9a1a95a4b39d9332e8fa62b42
abbas886 <abbas886@yahoo.com>
2018-05-26 10:21:56 +0530

    //use camel case

--- src/com/cts/program/CountLowerCase.java
+++ src/com/cts/program/CountLowerCase.java
@@ -1,7 +1,8 @@
 package com.cts.program;
 
 public class CountLowerCase {
-	
+	//use camel case
+	//format/align the source code before committing in the git.
 		public int lowercaseCount(String str) {
 			int lower=0 ,len=str.length();
 			 for(int i = 0; i < len; i++)
